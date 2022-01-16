a6110b3c7c55ffd5d85de8d18eaf6aba81f4cd87
Twiglard <basedpoland@example.invalid>
2022-01-16 22:29:36 +0100

    remove inaccurate comment

--- ship.hpp
+++ ship.hpp
@@ -19,8 +19,8 @@ struct ship final
     constexpr float horizontal_twr() const { return horizontal_thrust * 100.f / mass; }
     constexpr float combat_time() const { return fuel / fuel_flow; }
     constexpr float speed() const { return twr() * 90; }
-    constexpr float fuel_usage() const { return 3600 * 20 * fuel_flow / speed();  } // note: fuel usage is 20 times lower outside combat
     //constexpr float range() const { return 1e3f * fuel / fuel_usage(); } // TODO inaccurate
+    constexpr float fuel_usage() const { return 3600 * 20 * fuel_flow / speed();  }
 
     int count(const part& part) const;
     void add_part(const part& x, int count);
